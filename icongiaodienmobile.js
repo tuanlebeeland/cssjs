a8c5711329e8bc6bd380228f05021bebfe0725c8
tuanlebeeland <61408322+tuanlebeeland@users.noreply.github.com>
2020-02-25 14:52:02 +0700

    Add files via upload

---
+++ icongiaodienmobile.js
@@ -0,0 +1,37 @@
+<link href='https://raw.githack.com/linhfaceseo/cssjs/master/toolbar.css' rel='stylesheet'/><div id='toolbar'>
+        <ul>
+                <li>
+                        <a href='tel:0938740479' title=''>
+                                <img alt='' src='https://1.bp.blogspot.com/-GQMu1P-MKwQ/XXI_CQpGheI/AAAAAAAAAXk/IP0Epk0XAgkrUZZoGlQqmLZP38b3TzLlACLcBGAs/s1600/phone.png'/> <br/>Gọi điện
+                        </a>
+                </li>
+                <li>
+                    
+                        <a href='sms:0938740479;?&amp;body=Tôi muốn đăng ký nhận bảng giá. Tư vấn giúp tôi.' title=''>
+                                <img alt='' src='https://1.bp.blogspot.com/-TjuOFlwG7RQ/XXI_CINawsI/AAAAAAAAAXc/kWlqJ17ZK_0lGYAUgrX1L4YmdX_hgTN5QCLcBGAs/s1600/message.png'/> <br/>Nhắn Tin
+                        </a>
+                </li>
+                <li>
+                        <a href='https://zalo.me/0938740479' title=''>
+                                <img alt='' src='https://1.bp.blogspot.com/-NQOpvhlIeYE/XXI_C1IV9yI/AAAAAAAAAXo/jvcQ9dL5qTMIFrFubduMB6gZxS01TJlxQCLcBGAs/s1600/zalo.png'/> <br/>Chat zalo
+                        </a>
+                        </li>
+                <li>
+                        <a href='https://www.messenger.com/t/nguyenlinh.ceo.faceseo' title=''>
+                                <img alt='' src='https://1.bp.blogspot.com/-SN6kpH5e1N4/XXI_CVlV19I/AAAAAAAAAXg/vvI1ru_tk80YJoBBwvrDn12Q7_UNm--LgCLcBGAs/s1600/facebook.png'/> <br/>Chat facebook
+                        </a>
+                </li>
+                <div class='clearfix'/>
+        </ul>
+</div>
+<style>
+
+#toolbar ul li {
+    list-style-type: none!important;
+}
+
+@media screen and (max-width: 768px) and (min-width: 200px){
+#3lichatboz,#3lichatmobile,#divIframe3liChat{ display:none !important;} 
+}
+
+</style>
